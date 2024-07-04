5b1e52c29a293ef55e6741cc61e53a3bed5426c3
zhuzhengwei <zhengwei.zhu@fringtech.com>
2024-07-04 15:54:12 +0800

    feat:add settings的使用

--- component-lib/src/main/java/com/neteasecloud/componentlib/utils/Utils.java
+++ component-lib/src/main/java/com/neteasecloud/componentlib/utils/Utils.java
@@ -2,11 +2,18 @@ package com.neteasecloud.componentlib.utils;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
 
 import java.lang.reflect.InvocationTargetException;
 
 public final class Utils {
+    public static final String TAG = Utils.class.getSimpleName();
+    public static final int TRUE = 1;
+    public static final int FALSE = 0;
 
     @SuppressLint("StaticFieldLeak")
     private static Application sApplication;
@@ -79,5 +86,63 @@ public final class Utils {
         throw new NullPointerException("u should init first");
     }
 
+    /**
+     * 根据值的类型调用对应的方法保存数据
+     *
+     * @param key key
+     * @param obj value
+     *            <p>
+     *            /data/system/users/0/settings_system.xml
+     *            adb shell settings get system xxx
+     */
+    public static void put(String key, Object obj) {
+        ContentResolver resolver = Utils.getApp().getContentResolver();
+        if (obj instanceof String) {
+            Settings.System.putString(resolver, key, (String) obj);
+        } else if (obj instanceof Integer) {
+            Settings.System.putInt(resolver, key, (Integer) obj);
+        } else if (obj instanceof Boolean) {
+            Settings.System.putInt(resolver, key, (Boolean) obj ? TRUE : FALSE);
+        } else if (obj instanceof Float) {
+            Settings.System.putFloat(resolver, key, (Float) obj);
+        } else if (obj instanceof Long) {
+            Settings.System.putLong(resolver, key, (Long) obj);
+        } else {
+            Log.w(TAG, "put key = " + key + " value = " + obj + " not save");
+        }
+    }
+
+    /**
+     * 根据默认值得到保存的数据的具体类型，然后调用相对于的方法获取值
+     *
+     * @param key          key
+     * @param defaultValue 默认值
+     * @return value
+     */
+    public static Object get(String key, Object defaultValue) {
+        Object value = defaultValue;
+        ContentResolver resolver = Utils.getApp().getContentResolver();
+        try {
+            if (defaultValue instanceof String) {
+                String getValue = Settings.System.getString(resolver, key);
+                if (!TextUtils.isEmpty(getValue)) {
+                    value = getValue;
+                }
+            } else if (defaultValue instanceof Integer) {
+                value = Settings.System.getInt(resolver, key, (Integer) defaultValue);
+            } else if (defaultValue instanceof Boolean) {
+                value = Settings.System.getInt(resolver, key) == TRUE;
+            } else if (defaultValue instanceof Float) {
+                value = Settings.System.getFloat(resolver, key, (Float) defaultValue);
+            } else if (defaultValue instanceof Long) {
+                value = Settings.System.getLong(resolver, key, (Long) defaultValue);
+            } else {
+                Log.w(TAG, "not get key = " + key + " defaultValue = " + defaultValue);
+            }
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return value;
+    }
 
 }
